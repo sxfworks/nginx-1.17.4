56f7ca94c17313e5cc52f0a90af26c3c128d92ef
shangxiaofei <shangxiaofei@bytedance.com>
2019-09-27 16:33:38 +0800

    添加build脚本
    Change-Id: I9e7fd6d71c1b37683e94e8bbc4823a062a0d46b7

--- build.sh
+++ build.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
-
 echo "Begin compile nginx-1.17.4 ......"
 
-if [ $# != 1 ]
-then
-  echo "Usage: build.sh prefix_path"
-  exit -1
-fi
+nginx_root=`pwd`
+mkdir -p $nginx_root/output/deps
+mkdir -p $nginx_root/output/nginx
 
-if [[ $1 != /* ]]
-then
-  echo "Prefix should begin with char '/'"
-  exit -1
-fi
+# create tar target dir
+mkdir -p $nginx_root/output/deps/nginx-upload-module-2.3.0
+mkdir -p $nginx_root/output/deps/openssl-1.0.0a
+mkdir -p $nginx_root/output/deps/zlib-1.1.0
+mkdir -p $nginx_root/output/deps/pcre-8.00
 
 pushd ./deps
-
-tar -xvf nginx_upload_module-2.2.0.tar.gz -C nginx-upload-module-2.3.0
-tar -xvf openssl-1.0.0a.tar.gz -C openssl-1.0.0a
-tar -xvf zlib-1.1.0.tar.gz -C zlib-1.1.0
-tar -xvf pcre-8.00.tar.bz2 -C pcre-8.00
-
+tar -xvf nginx_upload_module-2.2.0.tar.gz -C $nginx_root/output/deps/nginx-upload-module-2.3.0
+tar -xvf openssl-1.0.0a.tar.gz -C $nginx_root/output/deps/openssl-1.0.0a
+tar -xvf zlib-1.1.0.tar.gz -C $nginx_root/output/deps/zlib-1.1.0
+tar -xvf pcre-8.00.tar.bz2 -C $nginx_root/output/deps/pcre-8.00
 popd
 
-echo ./configure --prefix=$1 \
+echo ./configure --prefix=$nginx_root/output/nginx \
             --with-file-aio \
             --with-threads \
             --with-http_ssl_module \
@@ -31,15 +26,15 @@ echo ./configure --prefix=$1 \
             --with-http_realip_module \
             --with-http_addition_module \
             --with-http_dav_module \
-            --with-http_flv_module \
+            --with-http_flv_module \ 
             --with-http_mp4_module \
             --with-http_gunzip_module \
             --with-http_gzip_static_module \
             --with-stream \
             --with-stream_ssl_module \
-            --with-openssl=../openssl-1.0.0a \
-            --with-pcre=../pcre-8.00 \
-            --with-zlib=../zlib-1.1.0 \
-            --add-module=../nginx-upload-module-2.3.0 \
+            --with-openssl=$nginx_root/output/deps/openssl-1.0.0a \
+            --with-pcre=$nginx_root/output/deps/pcre-8.00 \
+            --with-zlib=$nginx_root/output/deps/zlib-1.1.0 \
+            --add-module=$nginx_root/output/deps/nginx-upload-module-2.3.0 \
 
 echo "Complete compile nginx-1.17.4 success."
